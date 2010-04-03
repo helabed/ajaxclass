6fef3c859de9727f7826626a069d3a4066e122a9
Hani Elabed <hani.elabed@gmail.com>
2010-04-02 22:01:45 -0500

    work in progress

--- public/project3/index.js
+++ public/project3/index.js
@@ -1,34 +1,94 @@
-function ajax() {
-    var ajaxObj;
-    if(window.XMLHttpRequest) {
-        ajaxObj = new XMLHttpRequest();
-    } else {
-        ajaxObj = new ActiveXObject("Microsoft.XMLHTTP");
-    }
+
+function InProgressSpinner() {};
+
+InProgressSpinner.showInProgressImage = function( section_id ) {
+  var waitingImage = document.createElement("img");
+  waitingImage.setAttribute("src","/images/spinner_red_large.gif");
+  var tag_used = document.getElementById(section_id);
+  if( tag_used.nodeName == 'A' || tag_used.nodeName == 'a' ) {
+    document.getElementById(section_id).parentNode.appendChild(waitingImage);
+  } else {
+    document.getElementById(section_id).appendChild(waitingImage);
+  }
+  return waitingImage;
+}
+
+InProgressSpinner.hideInProgressImage = function( section_id, waitingImage ) {
+  var tag_used = document.getElementById(section_id);
+  if( tag_used.nodeName == 'A' || tag_used.nodeName == 'a' ) {
+    setTimeout( function() { document.getElementById(section_id).parentNode.removeChild(waitingImage);},  500 );
+  } else {
+    setTimeout( function() { document.getElementById(section_id).removeChild(waitingImage);},  500 );
+  }
+}
+
+
+
+
+
+
+function ajax(waitingImage, label_cell_id) {
+	var ajaxObj;
+	if(window.XMLHttpRequest) {
+	  ajaxObj = new XMLHttpRequest();
+	} else {
+    ajaxObj = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+			
+	ajaxObj.onreadystatechange = function() {
+			//0 open has not been called
+			//1 open has been called but not send
+			//2 send has been called but no response from the server
+			//3 data is in the process of being received
+			//4 response from the server  is ready to be processed
+			if(ajaxObj.readyState==4) {
+					
+				var myObj = JSON.parse(ajaxObj.responseText);
+				//alert(myObj.length);
+				//alert(typeof myObj);
+				//alert(myObj[0].car_make.name); 
+				//alert(myObj[1].car_make.name); 
+				//alert(myObj[2].car_make.name); 
+    		var car_makes = document.getElementById("carMakes");
+
+				var empty_car_make = document.createElement("option"); //<option></option>
+				empty_car_make.setAttribute("value", 0);//<option value="0"></option>
+				empty_car_make.appendChild(document.createTextNode("")); //<option value="0"></option>
+				car_makes.appendChild(empty_car_make);
     
-        
-    ajaxObj.onreadystatechange = function() {
-        //0 open has not been called
-        //1 open has been called but not send
-        //2 send has been called but no response from the server
-        //3 data is in the process of being received
-        //4 response from the server  is ready to be processed
-        if(ajaxObj.readyState==4) {
-            alert(ajaxObj.responseText);
+			  for( var i = 0; i < myObj.length; i++ ) {
+					var a_car_make = document.createElement("option"); //<option></option>
+					a_car_make.setAttribute("value", myObj[i].car_make.id);//<option value="1"></option>
+					a_car_make.appendChild(document.createTextNode(myObj[i].car_make.name)); //<option value="1">Toyota</option>
+					car_makes.appendChild(a_car_make);
+				}	
+
+
+
+
+          
+          InProgressSpinner.hideInProgressImage(label_cell_id, waitingImage);
+          document.getElementById("content").style.display = "";
         }
     }
     
     
     
     //open (method, url,async)
-    ajaxObj.open("get", "hello.php", true);
+    ajaxObj.open("get", "/car_makes.json", true);
 
     //send
     ajaxObj.send(null);
 }
 
 function init() {
+  //alert( "here");
+  var label_cell_id = "spinner";
+  var waitingImage = InProgressSpinner.showInProgressImage(label_cell_id);
     
+	ajax(waitingImage, label_cell_id);
+  
     
     //string of xml
     /*
