34d2b4bdc559c2675eb8a5d75f40fb14a91d1cf2
Hani Elabed <hani.elabed@gmail.com>
2010-04-04 10:24:27 -0500

    refactored javascript to DRY it

--- public/project3/index.js
+++ public/project3/index.js
@@ -4,11 +4,6 @@ function init() {
 }
 
 
-function populate_car_makes_selection() {
-  populate_selection_list( "carMakes", "/car_makes.json", "car_make", "id", "name" );
-}
-
-
 function init_car_makes_events_handler() {
   var car_makes = document.getElementById("carMakes");
   if( navigator.appName == "Microsoft Internet Explorer") {
@@ -19,174 +14,75 @@ function init_car_makes_events_handler() {
 }
 
 
-function init_car_models_events_handler() {
-  var car_models = document.getElementById("carModels");
-  if( navigator.appName == "Microsoft Internet Explorer") {
-    car_models.attachEvent("onchange", load_car_trims); // IE only
-  } else {
-    car_models.addEventListener("change", load_car_trims, false);
-  }
-}
-
-
-function populate_selection_list(select_id, request, json_obj, option_value, option_text) {
-  var spinner_id = "spinner";
-  var inProgressImage = InProgressSpinner.showInProgressImage(spinner_id);
-  var ajaxObj;
-  if(window.XMLHttpRequest) {
-    ajaxObj = new XMLHttpRequest();
-  } else {
-    ajaxObj = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  ajaxObj.onreadystatechange = function() {
-    if(ajaxObj.readyState==4) {
-      var myObj = JSON.parse(ajaxObj.responseText);
-      var select_list = document.getElementById(select_id);
-
-      var empty_option_tag = document.createElement("option"); //<option></option>
-      empty_option_tag.setAttribute("value", 0);//<option value="0"></option>
-      empty_option_tag.appendChild(document.createTextNode("")); //<option value="0"></option>
-      select_list.appendChild(empty_option_tag);
-      for( var i = 0; i < myObj.length; i++ ) {
-        var a_option_tag = document.createElement("option"); //<option></option>
-        var my_json_obj_i = myObj[i][json_obj];
-        a_option_tag.setAttribute("value", my_json_obj_i[option_value]);//<option value="1"></option>
-        a_option_tag.appendChild(document.createTextNode(my_json_obj_i[option_text])); //<option value="1">Toyota</option>
-        //a_option_tag.setAttribute("value", myObj[i].car_make.id);//<option value="1"></option>
-        //a_option_tag.appendChild(document.createTextNode(myObj[i].car_make.name)); //<option value="1">Toyota</option>
-        select_list.appendChild(a_option_tag);
-      }
-      InProgressSpinner.hideInProgressImage(spinner_id, inProgressImage);
-      document.getElementById("content").style.display = "";
-    }
-  }
-  //open (method, url,async)
-  ajaxObj.open("get", request, true);
-
-  //send
-  ajaxObj.send(null);
-}
-
-
-
-
-
-
-
 var update_selection_list_function = function () {
-	load_car_models();
-  //clear_selection_list("carModels");
-  //populate_selection_list( "carModels", "/car_models.json", "car_model", "id", "model" );
-
+  populate_car_models_selection();
 	clear_selection_list("carTrims");
 }
 
-var clear_selection_list = function (select_id) {
-  var old_select = document.getElementById(select_id);
-  if( old_select ) {
-    var parent_node_of_select = old_select.parentNode;
-    old_select.parentNode.removeChild(old_select);
-  }
-  var new_select = document.createElement("select");
-  new_select.setAttribute("id", select_id);
-  parent_node_of_select.appendChild(new_select);
+
+function populate_car_makes_selection() {
+  make_ajax_request("/car_makes.json", car_makes_callback)
 }
 
 
-var load_car_models = function() {
-	var spinner_id = "spinner";
-	var inProgressImage = InProgressSpinner.showInProgressImage(spinner_id);
-	var ajaxObj;
-	if(window.XMLHttpRequest) {
-		ajaxObj = new XMLHttpRequest();
-	} else {
-		ajaxObj = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	ajaxObj.onreadystatechange = function() {
-		if(ajaxObj.readyState==4) {
-			var myObj = JSON.parse(ajaxObj.responseText);
-			var old_car_models = document.getElementById("carModels");
-			if( old_car_models ) {
-				var parent_node_of_car_models = old_car_models.parentNode;
-				old_car_models.parentNode.removeChild(old_car_models);
-			}
-			var car_models = document.createElement("select");
-			car_models.setAttribute("id", "carModels");
-			car_models.className = "car_models_class";
+function car_makes_callback(result_obj) {
+  populate_selection_list( "carMakes", "car_make", "id", "name", result_obj );
+}
 
-			var empty_car_model = document.createElement("option"); //<option></option>
-			empty_car_model.setAttribute("value", 0);//<option value="0"></option>
-			empty_car_model.appendChild(document.createTextNode("")); //<option value="0"></option>
-			car_models.appendChild(empty_car_model);
 
-			for( var i = 0; i < myObj.length; i++ ) {
-				var a_car_model = document.createElement("option"); //<option></option>
-				a_car_model.setAttribute("value", myObj[i].car_model.id);//<option value="1"></option>
-				a_car_model.appendChild(document.createTextNode(myObj[i].car_model.model)); //<option value="1">prius</option>
-				car_models.appendChild(a_car_model);
-			}
-			parent_node_of_car_models.appendChild(car_models);
 
-			// we are doing the init here because the Car Trims Selection list depends on the Car Models Selection list
-			// and we need the latest version of the selection list element, not the one that was deleted above.
-			init_car_models_events_handler();
-
-			InProgressSpinner.hideInProgressImage(spinner_id, inProgressImage);
-		}
-	}
+function populate_car_models_selection() {
 	var selected_car_make_id = document.getElementById("carMakes").value;
-	//open (method, url,async)
-	ajaxObj.open("get", "/car_models.json?car_make_id="+selected_car_make_id, true);
-	//send
-	ajaxObj.send(null);
+  make_ajax_request("/car_models.json?car_make_id="+selected_car_make_id, car_models_callback)
 }
 
 
+function car_models_callback(result_obj) {
+	clear_selection_list("carModels");
+  populate_selection_list( "carModels", "car_model", "id", "model", result_obj );
+	init_car_models_events_handler();
+}
 
 
-var load_car_trims = function() {
-	var ajaxObj;
-	if(window.XMLHttpRequest) {
-		ajaxObj = new XMLHttpRequest();
-	} else {
-		ajaxObj = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	var spinner_id = "spinner";
-	var inProgressImage = InProgressSpinner.showInProgressImage(spinner_id);
-	ajaxObj.onreadystatechange = function() {
-		if(ajaxObj.readyState==4) {
-			var myObj = JSON.parse(ajaxObj.responseText);
-			var old_car_trims = document.getElementById("carTrims");
-			if( old_car_trims ) {
-				var parent_node_of_car_trims = old_car_trims.parentNode;
-				old_car_trims.parentNode.removeChild(old_car_trims);
-			}
-			var car_trims = document.createElement("select");
-			car_trims.setAttribute("id", "carTrims");
-			car_trims.className = "car_trims_class";
+function init_car_models_events_handler() {
+  var car_models = document.getElementById("carModels");
+  if( navigator.appName == "Microsoft Internet Explorer") {
+    car_models.attachEvent("onchange", populate_car_trims_selection); // IE only
+  } else {
+    car_models.addEventListener("change", populate_car_trims_selection, false);
+  }
+}
 
 
-			var empty_car_trim = document.createElement("option"); //<option></option>
-			empty_car_trim.setAttribute("value", 0);//<option value="0"></option>
-			empty_car_trim.appendChild(document.createTextNode("")); //<option value="0"></option>
-			car_trims.appendChild(empty_car_trim);
-
-			for( var i = 0; i < myObj.length; i++ ) {
-				var a_car_trim = document.createElement("option"); //<option></option>
-				a_car_trim.setAttribute("value", myObj[i].car_trim.id);//<option value="1"></option>
-				a_car_trim.appendChild(document.createTextNode(myObj[i].car_trim.trim)); //<option value="1">prius</option>
-				car_trims.appendChild(a_car_trim);
-			}
-			parent_node_of_car_trims.appendChild(car_trims);
-			InProgressSpinner.hideInProgressImage(spinner_id, inProgressImage);
-		}
-	}
+function populate_car_trims_selection() {
 	var selected_car_model_id = document.getElementById("carModels").value;
-	//open (method, url,async)
-	ajaxObj.open("get", "/car_trims.json?car_model_id="+selected_car_model_id, true);
-	//send
-	ajaxObj.send(null);
+  make_ajax_request("/car_trims.json?car_model_id="+selected_car_model_id, car_trims_callback)
 }
+
+
+function car_trims_callback(result_obj) {
+	clear_selection_list("carTrims");
+  populate_selection_list( "carTrims", "car_trim", "id", "trim", result_obj );
+	init_car_trims_events_handler();
+}
+
+function init_car_trims_events_handler() {
+  var car_models = document.getElementById("carTrims");
+  if( navigator.appName == "Microsoft Internet Explorer") {
+    car_models.attachEvent("onchange", car_trims_selection_changed); // IE only
+  } else {
+    car_models.addEventListener("change", car_trims_selection_changed, false);
+  }
+}
+
+function car_trims_selection_changed() {
+	var selected_car_trim_id = document.getElementById("carTrims").value;
+	var selected_car_trim_text = document.getElementById("carTrims").text;;
+  alert("you selected: "+selected_car_trim_id);
+  alert("you selected: "+selected_car_trim_text);
+}
+
+
 
 
 
@@ -214,3 +110,59 @@ InProgressSpinner.hideInProgressImage = function( section_id, waitingImage ) {
   }
 }
 
+
+var clear_selection_list = function (select_id) {
+  var old_select = document.getElementById(select_id);
+  if( old_select ) {
+    var parent_node_of_select = old_select.parentNode;
+    old_select.parentNode.removeChild(old_select);
+  }
+  var new_select = document.createElement("select");
+  new_select.setAttribute("id", select_id);
+  parent_node_of_select.appendChild(new_select);
+}
+
+
+function populate_selection_list(select_id, json_obj, option_value, option_text, myObj) {
+  var select_list = document.getElementById(select_id);
+
+  var empty_option_tag = document.createElement("option"); //<option></option>
+  empty_option_tag.setAttribute("value", 0);//<option value="0"></option>
+  empty_option_tag.appendChild(document.createTextNode("")); //<option value="0"></option>
+  select_list.appendChild(empty_option_tag);
+
+  for( var i = 0; i < myObj.length; i++ ) {
+    var a_option_tag = document.createElement("option"); //<option></option>
+    var my_json_obj_i = myObj[i][json_obj];
+    a_option_tag.setAttribute("value", my_json_obj_i[option_value]);//<option value="1"></option>
+    a_option_tag.appendChild(document.createTextNode(my_json_obj_i[option_text])); //<option value="1">Toyota</option>
+    //a_option_tag.setAttribute("value", myObj[i].car_make.id);//<option value="1"></option>
+    //a_option_tag.appendChild(document.createTextNode(myObj[i].car_make.name)); //<option value="1">Toyota</option>
+    select_list.appendChild(a_option_tag);
+  }
+  document.getElementById("content").style.display = "";
+}
+
+
+function make_ajax_request(request, call_back_function) {
+  var spinner_id = "spinner";
+  var inProgressImage = InProgressSpinner.showInProgressImage(spinner_id);
+  var ajaxObj;
+  if(window.XMLHttpRequest) {
+    ajaxObj = new XMLHttpRequest();
+  } else {
+    ajaxObj = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  ajaxObj.onreadystatechange = function() {
+    if(ajaxObj.readyState==4) {
+      var myObj = JSON.parse(ajaxObj.responseText);
+      call_back_function(myObj);
+      InProgressSpinner.hideInProgressImage(spinner_id, inProgressImage);
+    }
+  }
+  //open (method, url,async)
+  ajaxObj.open("get", request, true);
+
+  //send
+  ajaxObj.send(null);
+}
