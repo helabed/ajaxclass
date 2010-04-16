8343d8e6bae60d9bb27c1a45e2b60ab01b4f6aef
Hani Elabed <hani.elabed@gmail.com>
2010-04-15 20:57:17 -0500

    removed transient $

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$(document).ready( function() {$
+$(document).ready( function() {
   $("#btnAdd").click( function() {
     $.post(  "/tasks.json",
             {"username":$("input[name=user_username]").val(),
