24d0ec70cb38e98c24d70c8ad1bf58ac7bf76e2e
Hani Elabed <hani.elabed@gmail.com>
2010-05-02 18:10:54 -0500

    accordion now works, cannot show source code page

--- public/project4/js/index.js
+++ public/project4/js/index.js
@@ -1,19 +1,21 @@
-	$(function() {
-		var stop = false;
-		$("#accordion h3").click(function(event) {
-			if (stop) {
-				event.stopImmediatePropagation();
-				event.preventDefault();
-				stop = false;
-			}
-		});
-		$("#accordion").accordion({
-			header: "> div > h3"
-		}).sortable({
-			axis: "y",
-			handle: "h3",
-			stop: function(event, ui) {
-				stop = true;
-			}
-		});
-	});
+$(function() {
+  var stop = false;
+  $("#accordion h3").click(function(event) {
+    if (stop) {
+      event.stopImmediatePropagation();
+      event.preventDefault();
+      stop = false;
+    }
+  });
+  $("#accordion").accordion({
+    autoHeight: false,
+    header: "> div > h3"
+  }).sortable({
+    autoHeight: false,
+    axis: "y",
+    handle: "h3",
+    stop: function(event, ui) {
+      stop = true;
+    }
+  });
+});
