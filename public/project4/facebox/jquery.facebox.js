a733efbccc499dccd5cd0328325e47b9527a8754
Hani Elabed <hani.elabed@gmail.com>
2010-05-06 18:46:01 -0500

    minor bug fix to facebox plugin

--- public/project4/facebox/jquery.facebox.js
+++ public/project4/facebox/jquery.facebox.js
@@ -136,7 +136,8 @@
     reveal: function(data, klass) {
       $(document).trigger('beforeReveal.facebox')
       if (klass) $('#facebox .content').addClass(klass)
-      $('#facebox .content').append(data)
+      //$('#facebox .content').append(data)
+      $('#facebox .content').html(data)
       $('#facebox .loading').remove()
       $('#facebox .body').children().fadeIn('normal')
       $('#facebox').css('left', $(window).width() / 2 - ($('#facebox table').width() / 2))
